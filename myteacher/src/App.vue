1265d92ed3e40e2c5c5bad0d81b9bbff335d323d
tree <1263464917@qqcom>
2019-06-27 21:36:19 +0800

    vue

---
+++ myteacher/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <tututu/>
+  </div>
+</template>
+
+<script>
+import vvv from './components/aaa.vue';
+export default {
+  name: 'app',
+  components: {
+   tututu:vvv,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
