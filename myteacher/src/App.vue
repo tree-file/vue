70edea34fd01aa1824243263cb9e4e2075bd63e2
tree <1263464917@qqcom>
2019-07-04 21:51:53 +0800

    myteacher

--- myteacher/src/App.vue
+++ myteacher/src/App.vue
@@ -1,29 +1,49 @@
 <template>
-  <div id="app">
-    <el-menu
+  <div class="app">
+    <el-row>
+      <el-col :span="18">
+        <el-menu
       :default-active="active"
       class="el-menu-demo"
       mode="horizontal"
-      active-text-color="skyblue"
+      active-text-color="#58bc58"
       @select="handleSelect"
       router
     >
-      <el-menu-item v-for="item in pages" :key="item.name" :index="item.path">
-        
-        {{item.title}}
-        </el-menu-item>
-    
+      <el-menu-item v-for="item in pages" :key="item.name" :index="item.path">{{item.title}}</el-menu-item>
     </el-menu>
-    <!-- 路由配置匹配后，渲染对应的组件到router-view -->
+      </el-col>
+      <el-col :span="6" class="login" v-if="!logined">
+         <el-link href="#" @click.native.prevent.stop="goto('Reg')">注册</el-link>
+         <el-link href="#" @click.native.prevent.stop="goto('Login')">登录</el-link>
+      </el-col>
+      <el-col :span="6" class="login" v-else>
+        <el-link href="#" @click.native.prevent.stop="logout()">退出</el-link>
+      </el-col>
+    </el-row>
+    
     <router-view/>
+    <router-view name="bottom"/>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
-import {Menu,MenuItem} from 'element-ui';
+import {Menu, MenuItem, Row,Col,Link,Container,Header,Aside,Main,Form,FormItem,Button,Input} from 'element-ui';
 Vue.use(Menu);
 Vue.use(MenuItem);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Link);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Button);
+Vue.use(Input);
+import 'element-ui/lib/theme-chalk/index.css'
 export default {
   name: 'app',
   data(){
@@ -47,18 +67,33 @@ export default {
           name:'Mine'
         }
       ],
-      active:"/home"
+      active:"/home",
+      logined:false,
     };
   },
   methods: {
     handleSelect(index,indexPaht){
       console.log(index,indexPaht);
       this.active = index
+    },
+    goto(name){
+      this.$router.push({
+        name,
+        query:{username:'pangzi',paw:123456},
+        params:{a:100,b:200}
+      })
+    },
+    logout(){
+      this.logined = false;
+      localStorage.removeItem('username');
     }
   },
   created(){
-    let hash = window.location.hash;
+    let hash = window.location.hash.slice(1);
     this.active = hash;
+
+    let username = localStorage.getItem('username');
+    this.logined = username ? true :false;
   }
 }
 </script>
@@ -66,7 +101,10 @@ export default {
 <style>
 .active {
   font-weight: bold;
-  color: #f00
+  color: rgb(55, 0, 255)
 }
-
+.login {
+  text-align:right;line-height:60px;
+  a{margin-right:10px};
+}
 </style>
--- myteacher/src/main.js
+++ myteacher/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './js/';
+import router from './js';
+
+import axios from 'axios'
+Vue.prototype.$axios = axios;
+// 配置基础路径
+axios.defaults.baseURL = 'http://localhost:1904';
 
 Vue.config.productionTip = false
 
